3004352037d78898b903cbe349ba2b89753eceda
Bruno Cardoso <br1@MacBook-Pro-de-Bruno.local>
2019-12-17 10:18:35 +0100

    cambios iago

--- src/app/main/products-services/products-services.module.ts
+++ src/app/main/products-services/products-services.module.ts
@@ -3,13 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsServicesRoutingModule } from './products-services-routing.module';
 import { ProductsServicesMainComponent } from './products-services-main/products-services-main.component';
-<<<<<<< HEAD
 import { MatToolbarModule, MatButtonModule, MatTableModule } from '@angular/material';
 import {ReactiveFormsModule} from '@angular/forms';
-=======
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
 import { SharedModule } from 'src/app/shared/shared.module';
->>>>>>> 9c6dac128bd658fbcd039fd0d4ad1a832db37b02
 
 
 @NgModule({
@@ -19,12 +15,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     ProductsServicesRoutingModule,
     MatToolbarModule,
     MatButtonModule,
-<<<<<<< HEAD
     ReactiveFormsModule,
-    MatTableModule
-=======
+    MatTableModule,
     SharedModule
->>>>>>> 9c6dac128bd658fbcd039fd0d4ad1a832db37b02
   ]
 })
 export class ProductsServicesModule { }
